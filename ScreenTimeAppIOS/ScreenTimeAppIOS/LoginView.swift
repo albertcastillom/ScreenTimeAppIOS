2f458889436cf05eef83a5e5327c92fbf914eae2
Albert Castillo <Albert.castillom@gmail.com>
2026-07-20 15:47:49 -0700

    Finished MVP UI in swift UI

---
+++ ScreenTimeAppIOS/ScreenTimeAppIOS/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ScreenTimeAppIOS
+//
+//  Created by Albert Castillo on 7/20/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
